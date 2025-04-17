13036bfb31f06c86389fed9bc6481f1ab2891b4d
yeonjae02 <0yeonjae2@naver.com>
2025-04-17 12:00:03 +0900

    init : 프로젝트 초기 세팅

---
+++ src/main/java/com/howWeather/howWeather_backend/HowWeatherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.howWeather.howWeather_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowWeatherBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowWeatherBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howWeather/howWeather_backend/HowWeatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howWeather.howWeather_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowWeatherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
