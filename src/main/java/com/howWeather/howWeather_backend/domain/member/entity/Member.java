532cb9d8386ce0ded7b0775ceb6af880a99dd216
yeonjae02 <0yeonjae2@naver.com>
2025-04-18 15:31:25 +0900

    feat: 멤버 컬럼 추가 및 nullable 추가

--- src/main/java/com/howWeather/howWeather_backend/domain/member/entity/Member.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/member/entity/Member.java
@@ -11,13 +11,13 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
-    @Column(name="member_login_id", unique = true)
+    @Column(name="member_login_id", unique = true, nullable = false)
     private String loginId;
 
-    @Column(name="member_pw")
+    @Column(name="member_pw", nullable = false)
     private String password;
 
-    @Column(name="member_email", unique = true)
+    @Column(name="member_email", unique = true, nullable = false)
     private String email;
 
     @Column(name="member_nickname", nullable = false, length = 100)
@@ -26,4 +26,15 @@ public class Member {
     @Column(name="member_constitution", nullable = false)
     private int constitution;
 
+    @Column(name="age_group", nullable = false)
+    private int ageGroup;
+
+    @Column(name="body_type", nullable = false)
+    private int bodyType;
+
+    @Column(name="gender", nullable = false)
+    private int gender;
+
+    @Column(name="sensitivity", nullable = false)
+    private int sensitivity = -1; // init
 }
