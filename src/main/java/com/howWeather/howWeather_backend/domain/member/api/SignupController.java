948bc3542720f48e83442dc4e0029b13abe7af33
yeonjae02 <0yeonjae2@naver.com>
2025-04-25 23:59:14 +0900

    feat: 회원가입 컨트롤러 코드 구현

--- src/main/java/com/howWeather/howWeather_backend/domain/member/api/SignupController.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/member/api/SignupController.java
@@ -1,18 +1,27 @@
 package com.howWeather.howWeather_backend.domain.member.api;
 
+import com.howWeather.howWeather_backend.domain.member.dto.SignupRequestDto;
+import com.howWeather.howWeather_backend.domain.member.entity.Member;
+import com.howWeather.howWeather_backend.domain.member.repository.MemberRepository;
 import com.howWeather.howWeather_backend.domain.member.service.SignupService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/signup")
 public class SignupController {
     private final SignupService signupService;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public SignupController(SignupService signupService) {
-        this.signupService = signupService;
-    }
 
     @GetMapping("/email-exist-check")
     public ResponseEntity<?> isEmailExist(@RequestParam String email) {
@@ -31,5 +40,37 @@ public class SignupController {
         if (loginIdAlreadyExist)
             return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 사용 중인 아아디입니다.");
         return ResponseEntity.ok("사용 가능한 아이디입니다.");
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
+        if (memberRepository.findByEmail(signupRequestDto.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("해당 이메일로 가입한 계정이 이미 존재합니다.");
+        }
+
+        if (memberRepository.findByLoginId(signupRequestDto.getLoginId()).isPresent()) {
+            return ResponseEntity.badRequest().body("해당 아이디를 가진 계정이 이미 존재합니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(signupRequestDto.getPassword());
+
+        try {
+            Member member = Member.builder()
+                    .loginId(signupRequestDto.getLoginId())
+                    .password(encodedPassword)
+                    .email(signupRequestDto.getEmail())
+                    .nickname(signupRequestDto.getNickname())
+                    .constitution(signupRequestDto.getConstitution())
+                    .ageGroup(signupRequestDto.getAgeGroup())
+                    .bodyType(signupRequestDto.getBodyType())
+                    .gender(signupRequestDto.getGender())
+                    .sensitivity(-1)
+                    .build();
+
+            memberRepository.save(member);
+            return ResponseEntity.ok("회원가입이 성공하였습니다!");
+        } catch(Exception e) {
+            return ResponseEntity.internalServerError().body(e);
+        }
     }
 }
