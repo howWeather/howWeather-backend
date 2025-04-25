4407f49eb01af1f577ec67b1636b93a1014f9bd2
yeonjae02 <0yeonjae2@naver.com>
2025-04-25 23:50:26 +0900

    chore: 어노테이션 추가

--- src/main/java/com/howWeather/howWeather_backend/global/jwt/RefreshToken.java
+++ src/main/java/com/howWeather/howWeather_backend/global/jwt/RefreshToken.java
@@ -1,14 +1,14 @@
 package com.howWeather.howWeather_backend.global.jwt;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Builder(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Data
+@Entity
+@NoArgsConstructor
 public class RefreshToken {
     @Id
     private String username;
