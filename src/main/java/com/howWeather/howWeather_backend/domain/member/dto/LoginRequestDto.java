044b28a43a5ad41db78be85bef053a0a4303ec34
yeonjae02 <0yeonjae2@naver.com>
2025-04-28 22:54:22 +0900

    feat: DTO 생성 및 수정
    - 로그인 INPUT 데이터를 받아오는 DTO 생성
    - 회원가입 DTO에서 ROLE 필드 추가

--- src/main/java/com/howWeather/howWeather_backend/domain/member/dto/LoginRequestDto.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/member/dto/LoginRequestDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginDto {
+public class LoginRequestDto {
     @NotBlank
     private String loginId;
 
