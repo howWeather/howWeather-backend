52550605900bdc7a13c172d342a1025112dfcd0d
yeonjae02 <0yeonjae2@naver.com>
2025-04-29 20:35:48 +0900

    feat: 커스텀 에러 추가

--- src/main/java/com/howWeather/howWeather_backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/howWeather/howWeather_backend/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.howWeather.howWeather_backend.global.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,5 +16,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e) {
         return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다.");
+    }
+
+    @ExceptionHandler(LoginException.class)
+    public ResponseEntity<String> handleLoginException(LoginException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(e.getMessage() + " (Error Code: " + e.getErrorCode() + ")");
     }
 }
