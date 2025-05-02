a7364209cecc285fb25307f5729c5176f1b65e7e
yeonjae02 <0yeonjae2@naver.com>
2025-05-02 16:48:45 +0900

    feat: 요청 데이터 검증을 위한 @Valid 어노테이션 추가

--- src/main/java/com/howWeather/howWeather_backend/domain/closet/dto/AddClothesDto.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/closet/dto/AddClothesDto.java
@@ -1,11 +1,15 @@
 package com.howWeather.howWeather_backend.domain.closet.dto;
 
+import jakarta.validation.Valid;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class AddClothesDto {
+    @Valid
     private List<ClothDto> uppers;
+
+    @Valid
     private List<ClothDto> outers;
 }
