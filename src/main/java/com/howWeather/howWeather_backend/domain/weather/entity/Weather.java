e676fe5247e4afbd1ca7c7f99edbc121a511043c
yeonjae02 <0yeonjae2@naver.com>
2025-05-14 23:44:42 +0900

    feat: 엔티티 생성

--- src/main/java/com/howWeather/howWeather_backend/domain/weather/entity/Weather.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/weather/entity/Weather.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Weather {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
