bdfa5a1732c114694c1b9e2cc112b7075b3e950e
yeonjae02 <0yeonjae2@naver.com>
2025-05-24 21:34:26 +0900

    feat: 반환 형식 구체화

--- src/main/java/com/howWeather/howWeather_backend/domain/alarm/api/AlarmController.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/alarm/api/AlarmController.java
@@ -5,7 +5,10 @@ import com.howWeather.howWeather_backend.domain.alarm.dto.FcmTokenRequestDto;
 import com.howWeather.howWeather_backend.domain.alarm.service.FcmAlarmPreferenceService;
 import com.howWeather.howWeather_backend.domain.alarm.service.FcmTokenService;
 import com.howWeather.howWeather_backend.domain.member.entity.Member;
+import com.howWeather.howWeather_backend.global.Response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +21,12 @@ public class AlarmController {
     private final FcmAlarmPreferenceService fcmAlarmPreferenceService;
 
     @PostMapping("/token-save")
-    public ResponseEntity<String> saveToken(@RequestBody FcmTokenRequestDto request,
+    public ResponseEntity<ApiResponse<String>> saveToken(@Valid @RequestBody FcmTokenRequestDto request,
                                           @RequestHeader("Authorization") String accessTokenHeader,
                                           @AuthenticationPrincipal Member member) {
         Long memberId = member.getId();
         fcmTokenService.saveToken(memberId, request.getToken());
-        return ResponseEntity.ok("FCM 토큰이 성공적으로 저장되었습니다.");
+        return ApiResponse.success(HttpStatus.OK, "FCM 토큰이 성공적으로 저장되었습니다.");
     }
 
     @DeleteMapping("/token-delete")
@@ -43,9 +46,9 @@ public class AlarmController {
     }
 
     @PatchMapping("/update")
-    public ResponseEntity<String> updatePreference(@AuthenticationPrincipal Member member,
-                                                 @RequestBody FcmAlarmPreferenceDto dto) {
+    public ResponseEntity<ApiResponse<String>> updatePreference(@AuthenticationPrincipal Member member,
+                                                  @RequestBody FcmAlarmPreferenceDto dto) {
         fcmAlarmPreferenceService.updatePreference(member, dto);
-        return ResponseEntity.ok("알람 수신 여부를 변경하였습니다.");
+        return ApiResponse.success(HttpStatus.OK, "알람 수신 여부를 변경하였습니다.");
     }
 }
