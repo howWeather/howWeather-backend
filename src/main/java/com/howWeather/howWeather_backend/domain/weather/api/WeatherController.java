88d29e319debf3add2499870fd700a009e705bf9
yeonjae02 <0yeonjae2@naver.com>
2025-05-25 22:33:24 +0900

    feat: 온도 반환 로직 구현

--- src/main/java/com/howWeather/howWeather_backend/domain/weather/api/WeatherController.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/weather/api/WeatherController.java
@@ -1,4 +1,27 @@
 package com.howWeather.howWeather_backend.domain.weather.api;
 
+import com.howWeather.howWeather_backend.domain.weather.dto.WeatherSimpleDto;
+import com.howWeather.howWeather_backend.domain.weather.service.WeatherService;
+import com.howWeather.howWeather_backend.global.Response.ApiResponse;
+import com.howWeather.howWeather_backend.global.jwt.CheckAuthenticatedUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/weather")
 public class WeatherController {
+    private final WeatherService weatherService;
+
+    @GetMapping("/temp")
+    @CheckAuthenticatedUser
+    public ResponseEntity<ApiResponse<Double>> getTemp(@RequestHeader("Authorization") String accessTokenHeader,
+                                                @RequestBody WeatherSimpleDto weatherSimpleDto) {
+        double temperature = weatherService.getTemperature(weatherSimpleDto);
+        return ApiResponse.success(HttpStatus.OK, temperature);
+    }
 }
