95bc5adf19e551b1b220862a92fcea83fbd5af7e
yeonjae02 <0yeonjae2@naver.com>
2025-05-29 23:43:30 +0900

    feat: 소셜 로그인 처리용 서비스 코드 구현

--- src/main/java/com/howWeather/howWeather_backend/domain/member/service/CustomOAuth2UserService.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/member/service/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package com.howWeather.howWeather_backend.domain.member.service;
 
+import com.howWeather.howWeather_backend.domain.member.dto.GoogleUserInfo;
 import com.howWeather.howWeather_backend.domain.member.dto.KakaoUserInfo;
+import com.howWeather.howWeather_backend.domain.member.dto.OAuth2UserInfo;
 import com.howWeather.howWeather_backend.domain.member.entity.LoginType;
 import com.howWeather.howWeather_backend.domain.member.entity.Member;
 import com.howWeather.howWeather_backend.domain.member.repository.MemberRepository;
@@ -27,32 +29,41 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        log.info("OAuth2User 정보 수신: {}", oAuth2User.getAttributes());
-
-        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "kakao"
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "kakao" or "google"
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        KakaoUserInfo kakaoUserInfo = new KakaoUserInfo(attributes);
+        OAuth2UserInfo userInfo = getOAuth2UserInfo(registrationId, attributes);
+        log.info("{} 로그인 사용자 정보: {}", registrationId, attributes);
 
-        Member member = memberRepository.findByEmail(kakaoUserInfo.getEmail())
-                .orElseGet(() -> registerNewUser(kakaoUserInfo));
+        String loginId = registrationId.substring(0, 1).toUpperCase() + registrationId.substring(1) + "_" + userInfo.getId();
+
+        Member member = memberRepository.findByLoginId(loginId)
+                .orElseGet(() -> registerNewUser(registrationId, loginId, userInfo));
 
         return new CustomOAuth2User(member, attributes, "id");
     }
 
-    private Member registerNewUser(KakaoUserInfo userInfo) {
-        String kakaoId = "Kakao_" + userInfo.getId();
+    private OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        return switch (registrationId) {
+            case "kakao" -> new KakaoUserInfo(attributes);
+            case "google" -> new GoogleUserInfo(attributes);
+            default -> throw new OAuth2AuthenticationException("지원하지 않는 소셜 로그인입니다: " + registrationId);
+        };
+    }
 
+    private Member registerNewUser(String registrationId, String loginId, OAuth2UserInfo userInfo) {
         Member member = Member.builder()
-                .loginId(kakaoId)
+                .loginId(loginId)
                 .password(UUID.randomUUID().toString())
                 .email(userInfo.getEmail())
                 .nickname(userInfo.getNickname())
-                .loginType(LoginType.KAKAO)
+                .loginType(LoginType.valueOf(registrationId.toUpperCase()))
+                .roles(List.of("ROLE_USER"))
                 .constitution(1)
                 .ageGroup(1)
                 .gender(1)
-                .roles(List.of("ROLE_USER"))
+                .sensitivity(1)
+                .isDeleted(false)
                 .build();
         return memberRepository.save(member);
     }
