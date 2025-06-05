29eb1fcff3d4e24cf7ec1a51880d73ff9bd176b5
yeonjae02 <0yeonjae2@naver.com>
2025-06-05 20:49:00 +0900

    refactor: 변경된 dto명 반영

--- src/main/java/com/howWeather/howWeather_backend/domain/ai_model/service/RecommendationService.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/ai_model/service/RecommendationService.java
@@ -1,9 +1,11 @@
 package com.howWeather.howWeather_backend.domain.ai_model.service;
 
-import com.howWeather.howWeather_backend.domain.ai_model.dto.ClothingRecommendationDto;
-import com.howWeather.howWeather_backend.domain.ai_model.dto.RecommendationResult;
+import com.howWeather.howWeather_backend.domain.ai_model.dto.ModelClothingRecommendationDto;
+import com.howWeather.howWeather_backend.domain.ai_model.dto.ModelRecommendationResult;
+import com.howWeather.howWeather_backend.domain.ai_model.dto.RecommendListDto;
 import com.howWeather.howWeather_backend.domain.ai_model.entity.ClothingRecommendation;
 import com.howWeather.howWeather_backend.domain.ai_model.repository.ClothingRecommendationRepository;
+import com.howWeather.howWeather_backend.domain.member.entity.Member;
 import com.howWeather.howWeather_backend.global.exception.CustomException;
 import com.howWeather.howWeather_backend.global.exception.ErrorCode;
 import lombok.AllArgsConstructor;
@@ -20,9 +22,9 @@ public class RecommendationService {
     private final ClothingRecommendationRepository clothingRecommendationRepository;
 
     @Transactional
-    public void save(ClothingRecommendationDto dto) {
+    public void save(ModelClothingRecommendationDto dto) {
         try {
-            for (RecommendationResult result : dto.getResult()) {
+            for (ModelRecommendationResult result : dto.getResult()) {
                 ClothingRecommendation recommendation = ClothingRecommendation.builder()
                         .memberId(dto.getUserId())
                         .tops(result.getTops())
@@ -37,5 +39,10 @@ public class RecommendationService {
             log.error("모델로부터 예측 결과를 받아오던 중 중 알 수 없는 에러 발생", e);
             throw new CustomException(ErrorCode.UNKNOWN_ERROR, "예측 결과를 AI로부터 받아오던 중 오류가 발생했습니다.");
         }
+    }
+
+    @Transactional(readOnly = true)
+    public RecommendListDto getRecommendList(Member member) {
+        return null;
     }
 }
