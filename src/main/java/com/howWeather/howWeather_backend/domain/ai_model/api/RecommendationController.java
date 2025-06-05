244fc1b7a58c94aae44cda0ca7c30486256232f6
yeonjae02 <0yeonjae2@naver.com>
2025-06-05 20:49:15 +0900

    feat: 컨트롤러 코드 작성

---
+++ src/main/java/com/howWeather/howWeather_backend/domain/ai_model/api/RecommendationController.java
@@ -0,0 +1,30 @@
+package com.howWeather.howWeather_backend.domain.ai_model.api;
+
+import com.howWeather.howWeather_backend.domain.ai_model.dto.RecommendListDto;
+import com.howWeather.howWeather_backend.domain.ai_model.service.RecommendationService;
+import com.howWeather.howWeather_backend.domain.member.entity.Member;
+import com.howWeather.howWeather_backend.global.Response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/model")
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<RecommendListDto>> getRecommendList(@RequestHeader("Authorization") String accessTokenHeader,
+                                                                          @AuthenticationPrincipal Member member) {
+        RecommendListDto recommendList = recommendationService.getRecommendList(member);
+        return ApiResponse.success(HttpStatus.OK, recommendList);
+    }
+}
