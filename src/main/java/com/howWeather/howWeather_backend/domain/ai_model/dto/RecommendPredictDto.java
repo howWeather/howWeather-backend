dede964f8350fe057615858f9d4a0e26ad737a1e
yeonjae02 <0yeonjae2@naver.com>
2025-06-05 21:54:28 +0900

    chore: 개발 편의를 위한 어노테이션 추가

--- src/main/java/com/howWeather/howWeather_backend/domain/ai_model/dto/RecommendPredictDto.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/ai_model/dto/RecommendPredictDto.java
@@ -1,10 +1,12 @@
 package com.howWeather.howWeather_backend.domain.ai_model.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class RecommendPredictDto {
     private List<Integer> uppersTypeList;
     private List<Integer> outersTypeList;
