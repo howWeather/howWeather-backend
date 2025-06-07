78cd1d677ed2fb944aa1667da532cce76bab1dba
yeonjae02 <0yeonjae2@naver.com>
2025-06-07 12:54:56 +0900

    fix: 충돌 해결을 위해 원상복구

--- src/main/java/com/howWeather/howWeather_backend/global/config/RestTemplateConfig.java
+++ src/main/java/com/howWeather/howWeather_backend/global/config/RestTemplateConfig.java
@@ -2,7 +2,6 @@ package com.howWeather.howWeather_backend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,9 +9,6 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(3000);
-        factory.setReadTimeout(5000);
-        return new RestTemplate(factory);
+        return new RestTemplate();
     }
 }
