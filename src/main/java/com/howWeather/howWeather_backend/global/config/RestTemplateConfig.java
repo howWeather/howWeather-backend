241d71607c1f3ccce438ca55d9b8bfd776f034b4
yeonjae02 <0yeonjae2@naver.com>
2025-06-07 12:31:26 +0900

    feat: 타임 아웃 설정

--- src/main/java/com/howWeather/howWeather_backend/global/config/RestTemplateConfig.java
+++ src/main/java/com/howWeather/howWeather_backend/global/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package com.howWeather.howWeather_backend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,9 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(5000);
+        return new RestTemplate(factory);
     }
 }
