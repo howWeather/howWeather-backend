cf549e5bf6719010d1de5a6704346b59eef61e2c
yeonjae02 <0yeonjae2@naver.com>
2025-06-07 09:29:06 +0900

    fix: 에러 해결을 위해 로그 출력 및 문제 발생 시 빈 리스트 반환하도록 수정

--- src/main/java/com/howWeather/howWeather_backend/domain/ai_model/api/RecommendationController.java
+++ src/main/java/com/howWeather/howWeather_backend/domain/ai_model/api/RecommendationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -26,8 +27,13 @@ public class RecommendationController {
     @GetMapping("/recommendation")
     public ResponseEntity<ApiResponse<List<RecommendPredictDto>>> getRecommendList(@RequestHeader("Authorization") String accessTokenHeader,
                                                                                    @AuthenticationPrincipal Member member) {
+        try {
+            List<RecommendPredictDto> recommendList = recommendationService.getRecommendList(member);
+            return ApiResponse.success(HttpStatus.OK, recommendList);
+        } catch (Exception e ){
+            log.error(e.getMessage());
+            return ApiResponse.success(HttpStatus.OK, new ArrayList<>());
+        }
 
-        List<RecommendPredictDto> recommendList = recommendationService.getRecommendList(member);
-        return ApiResponse.success(HttpStatus.OK, recommendList);
     }
 }
