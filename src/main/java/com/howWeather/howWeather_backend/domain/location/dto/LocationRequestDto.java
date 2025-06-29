4b72818452aff884dda3b290f0425855907e127f
yeonjae02 <0yeonjae2@naver.com>
2025-06-30 06:20:46 +0900

    feat: 위경도 데이터를 받아 지역명 및 온도 반환 dto 수정

--- src/main/java/com/howWeather/howWeather_backend/domain/location/dto/LocationRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.howWeather.howWeather_backend.domain.location.dto;
-
-import lombok.Data;
-
-@Data
-public class LocationRequestDto {
-    private double latitude;
-    private double longitude;
-}
