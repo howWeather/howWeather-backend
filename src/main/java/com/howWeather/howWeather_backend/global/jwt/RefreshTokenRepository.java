6a801d991d3300df576f575125e921cb3af00bf7
yeonjae02 <0yeonjae2@naver.com>
2025-07-31 18:57:28 +0900

    chore: 미사용 파일 삭제

--- src/main/java/com/howWeather/howWeather_backend/global/jwt/RefreshTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.howWeather.howWeather_backend.global.jwt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-}
